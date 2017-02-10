f936e39a2e2694f96254aae348df381e08681d4c
Alan Said <alan@alans.se>
2017-02-10 15:21:44 +0100

    Post per year extractor

--- UserPostCountExtractor.py
+++ UserPostCountExtractor.py
@@ -5,10 +5,10 @@ import xml.etree.ElementTree
 
 
 def main():
-    minposts = 10
-    year = 2014
-    # users = extractUsers(minposts, year)
-    extractComments()
+    minposts = 50
+    years = [2013]
+    users = extractUsers(minposts, years)
+    # extractComments()
 
 def extractComments():
     users = set()
@@ -54,38 +54,40 @@ def extractComments():
         line = rowID + '\t' + creationDate + '\t' + postTypeId + '\t' + score + '\t' + title + '\t' + text + '\t' + tags + '\n'
         postsFile.write(line)
 
-def extractUsers(minPostCount, year):
-    posts = open("../2014-posts.xml", 'r')
+def extractUsers(minPostCount, years):
     users = {}
-    for post in posts:
-        post = post.rstrip('\n')
-        if "row Id" not in post:
-            continue
-        doc = xml.etree.ElementTree.fromstring(post)
-        creationDate = doc.get('CreationDate')
-        if str(year) not in creationDate:
-            continue
-        # rowID = doc.get('Id')
-        # postTypeId = doc.get('PostTypeId')
-        # acceptedAnswerId = doc.get('AcceptedAnswerId')
-        # score = doc.get('Score')
-        # viewCount = doc.get('ViewCount')
-        # text = doc.get('Body').encode('utf8')
-        ownerUserID = doc.get('OwnerUserId')
-        # lastEditorUserID = doc.get('LastEditorUserId')
-        # lastEditorDisplayName = doc.get('LastEditorDisplayName')
-        # lastEditDate = doc.get('LastEditDate')
-        # lastActivityDate = doc.get('LastActivityDate')
-        # title = doc.get('Title').encode('utf8')
-        # tags = doc.get('Tags').encode('utf8')
-        # answerCount = doc.get('AnswerCount')
-        # commentCount = doc.get('CommentCount')
-        # favoriteCount = doc.get('FavoriteCount')
-        # communityOwnedDate = doc.get('CommunityOwnedDate')
-        if ownerUserID in users:
-            users[ownerUserID] = users[ownerUserID] + 1
-        else:
-            users[ownerUserID] = 1
+    for year in years:
+        print "Parsing year: " +str(year)
+        posts = open("../"+str(year)+"-Posts.xml", 'r')
+        for post in posts:
+            post = post.rstrip('\n')
+            if "row Id" not in post:
+                continue
+            doc = xml.etree.ElementTree.fromstring(post)
+            creationDate = doc.get('CreationDate')
+            if int(creationDate[:4]) not in years:
+                continue
+            # rowID = doc.get('Id')
+            # postTypeId = doc.get('PostTypeId')
+            # acceptedAnswerId = doc.get('AcceptedAnswerId')
+            # score = doc.get('Score')
+            # viewCount = doc.get('ViewCount')
+            # text = doc.get('Body').encode('utf8')
+            ownerUserID = doc.get('OwnerUserId')
+            # lastEditorUserID = doc.get('LastEditorUserId')
+            # lastEditorDisplayName = doc.get('LastEditorDisplayName')
+            # lastEditDate = doc.get('LastEditDate')
+            # lastActivityDate = doc.get('LastActivityDate')
+            # title = doc.get('Title').encode('utf8')
+            # tags = doc.get('Tags').encode('utf8')
+            # answerCount = doc.get('AnswerCount')
+            # commentCount = doc.get('CommentCount')
+            # favoriteCount = doc.get('FavoriteCount')
+            # communityOwnedDate = doc.get('CommunityOwnedDate')
+            if ownerUserID in users:
+                users[ownerUserID] = users[ownerUserID] + 1
+            else:
+                users[ownerUserID] = 1
 
     userPosts = open("../userposts.txt", 'a')
     for user in users:
@@ -94,6 +96,6 @@ def extractUsers(minPostCount, year):
     userPosts.close()
     return set(users.keys())
 
-    if __name__ == '__main__':
-        main()
+if __name__ == '__main__':
+    main()
 
