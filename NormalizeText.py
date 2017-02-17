8387cdc4e42b5eab3b2b9ae78b4427b29de701c8
Alan Said <alan@alans.se>
2017-02-17 09:53:50 +0100

    Cleanup

--- NormalizeText.py
+++ NormalizeText.py
@@ -1,29 +1,39 @@
 from gensim import utils, corpora, models
-import logging, json
+from gensim.parsing.preprocessing import STOPWORDS
+import logging, json, os
 from pprint import pprint
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.DEBUG)
-import nltk
+from nltk.stem.porter import PorterStemmer
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+
+# import nltk
 
 
-class MyCorpus(object):
-    def __iter__(self):
-        for line in open(file):
-            dictionary = corpora.Dictionary.load('models/global-dictionary.dict')
-            yield dictionary.doc2bow(utils.lemmatize(line.split('\t')[5].lower(), stopwords=stoplist))
-
+# class MyCorpus(object):
+#     def __iter__(self):
+#         for line in open(file):
+#             dictionary = corpora.Dictionary.load('models/global-dictionary.dict')
+#             # stoplist = set(nltk.corpus.stopwords.words("english"))
+#             stoplist = STOPWORDS
+#             yield dictionary.doc2bow(utils.lemmatize(line.split('\t')[5].lower(), stopwords=stoplist))
+#
 
 def main():
-    date = '2013-00'
-    file = 'data/'+date+'-posts.tsv'
-    ids = [14109483] #, 14108285]
+    # months = [str(year) + "-" + str(item).zfill(2) for item in range(1, 3)]
 
-    createGlobalDictionary()
-    # memoryBasedLemmatization(date, file)
-    # performTFIDF(date)
-    # performLDA(date)
+    dates = ['2013-00']#, '2013-02']
+    # file = 'data/'+date+'-posts.tsv'
+    # ids = [14109483] #, 14108285]
 
-    # lookupLDATopics(date, ids)
+    # createGlobalDictionary()
+    memoryBasedTokenization(dates)
+    performTFIDF(dates)
+    performLDA(dates)
+    # topics = lookupLDATopics(date, ids, 5)
 
+    # performHDP(date)
+    # lookupHDPTopics(date, ids)
 
 def readFile(date):
     original_sentences = {}
@@ -33,73 +43,133 @@ def readFile(date):
     return original_sentences
 
 
-def lookupLDATopics(date, docIDs):
-    lemmatized_dict = json.load(file("models/" + date + "-lemmatized_dict.json"))
+
+def lookupHDPTopics(date, docIDs):
+    tokenized_dict = json.load(file("models/global-tokenized_dict.json"))
     original_sentences = readFile(date)
-    dictionary = corpora.Dictionary.load("models/" + date + "-dictionary.dict")
-    lda = models.LdaModel.load("models/"+date+"-lda.model")
+    dictionary = corpora.Dictionary.load("models/global-dictionary.dict")
+    hdp = models.HdpModel.load("models/"+date+"-hdp.model")
     for docID in docIDs:
-        # print lemmatized_dict[str(docID)]
-        sentence = lemmatized_dict[str(docID)]
+        sentence = tokenized_dict[str(docID)]
         bow = dictionary.doc2bow(sentence)
         pprint(sentence)
         pprint(original_sentences[str(docID)])
 
-        print(lda[bow])
+        print(hdp[bow])
+
+    print hdp.show_topics(num_topics=-1, num_words=5, formatted=True)
+
+
+def lookupLDATopics(date, docIDs, numTopics):
+    tokenized_dict = json.load(file("models/global-tokenized_dict.json"))
+    dictionary = corpora.Dictionary.load("models/global-dictionary.dict")
+    lda = models.LdaModel.load("models/"+date+"-lda.model")
+    for docID in docIDs:
+        sentence = tokenized_dict[str(docID)]
+        bow = dictionary.doc2bow(sentence)
+        topics = lda[bow]
+        topics_by_value = sorted(topics, key=lambda tup: tup[1], reverse=True)
+        return topics_by_value[:numTopics]
 
 
 
-def performTFIDF(date):
-    corpus = corpora.MmCorpus("models/" + date + "-lemmatized.mm")
-    tfidf = models.TfidfModel(corpus)
-    tfidf.save("models/"+date+"-tfidf.model")
-    tfidf_corpus = tfidf[corpus]
-    corpora.MmCorpus.save_corpus("models/"+date+"-tfidf.mm", tfidf_corpus)
 
-def performLDA(date):
-    dictionary = corpora.Dictionary.load("models/" + date + "-dictionary.dict")
+def performTFIDF(dates):
+    for date in dates:
+        corpus = corpora.MmCorpus("models/" + date + "-tokenized.mm")
+        tfidf = models.TfidfModel(corpus)
+        tfidf.save("models/"+date+"-tfidf.model")
+        tfidf_corpus = tfidf[corpus]
+        corpora.MmCorpus.save_corpus("models/"+date+"-tfidf.mm", tfidf_corpus)
+
+def performLDA(dates):
+    for date in dates:
+        print "performing lda"
+        dictionary = corpora.Dictionary.load("models/global-dictionary.dict")
+        corpus = corpora.MmCorpus("models/" + date + "-tfidf.mm")
+        lda = models.LdaMulticore(corpus, id2word=dictionary, num_topics=20, workers=3)
+        # lda = models.LdaModel(corpus, alpha='auto', id2word=dictionary, num_topics=20)
+        lda_corpus = lda[corpus]
+        corpora.MmCorpus.serialize('models/' + date + '-lda.mm', lda_corpus)
+        lda.save('models/' + date + '-lda.model')
+
+def performHDP(date):
+    print "performing lda"
+    dictionary = corpora.Dictionary.load("models/global-dictionary.dict")
     corpus = corpora.MmCorpus("models/" + date + "-tfidf.mm")
-    lda = models.LdaModel(corpus, alpha='auto', id2word=dictionary, num_topics=20)
-    lda_corpus = lda[corpus]
-    corpora.MmCorpus.serialize('models/' + date + '-lda.mm', lda_corpus)
-    lda.save('models/' + date + '-lda.model')
+    hdp = models.HdpModel(corpus, id2word=dictionary)
+    hdp_corpus = hdp[corpus]
+    corpora.MmCorpus.serialize('models/' + date + '-hdp.mm', hdp_corpus)
+    hdp.save('models/' + date + '-hdp.model')
+
 
 def createGlobalDictionary():
-    years = [2013, 2014]
-    stoplist = set(nltk.corpus.stopwords.words("english"))
-    lemmatized_dict = {}
+    years = [2013] #, 2014]
+    # stoplist = set(nltk.corpus.stopwords.words("english"))
+    stoplist = STOPWORDS
+    tokenized_dict = {}
     original_dict = {}
     for year in years:
-        months = [str(year) + "-" + str(item).zfill(2) for item in range(1, 13)]
+        # months = [str(year) + "-" + str(item).zfill(2) for item in range(1, 13)]
+        months = ['2013-00']
         for month in months:
             print "parsing month: " + month
             for line in open("data/" + month + "-posts.tsv"):
                 [id, postDate, type, score, title, text, tags] = line.split('\t')
-                lemmatized_line = utils.lemmatize(text.lower(), stopwords=stoplist)
-                lemmatized_dict[id] = lemmatized_line
+                tokenized_line = utils.lemmatize(text.lower(), stopwords=stoplist)
+                tokenized_dict[id] = tokenized_line
                 original_dict[id] = text
 
-    with open("models/global-lemmatized_dict.json", 'w') as f: f.write(json.dumps(lemmatized_dict))
+    with open("models/global-tokenized_dict.json", 'w') as f: f.write(json.dumps(tokenized_dict))
     with open("models/global-original_dict.json", 'w') as f: f.write(json.dumps(original_dict))
-    dictionary = corpora.Dictionary(lemmatized_dict.values())
+    dictionary = corpora.Dictionary(tokenized_dict.values())
     dictionary.compactify()
     dictionary.save('models/global-dictionary.dict')
 
 
-def fileBasedLemmatization(date, file):
-    corpus = MyCorpus() # doesn't load the corpus into memory!
-    corpora.MmCorpus.serialize('models/'+date+'-corpus.mm', corpus)
+
+# def memoryBasedStemming():
 
 
-def memoryBasedLemmatization(date, file):
-    stoplist = set(nltk.corpus.stopwords.words("english"))
-    lemmatized_dict = {}
-    for line in open(file):
-        [id, postDate, type, score, title, text, tags] = line.split('\t')
-        lemmatized_line = utils.lemmatize(text.lower(), stopwords=stoplist)
-    dictionary = corpora.Dictionary.load('models/global-dictionary.dict')
-    corpus = [dictionary.doc2bow(sentence) for sentence in lemmatized_dict.values()]
-    corpora.MmCorpus.serialize('models/'+date+'-lemmatized.mm', corpus)  # store to disk, for later use
+def memoryBasedTokenization(dates):
+    stoplist = set(stopwords.words("english"))
+
+    stemmed_or_tokenized_file = "models/global-tokenized_dict.json"
+    if os.path.isfile(stemmed_or_tokenized_file):
+        stemmed_or_tokenized_dict = json.load(file(stemmed_or_tokenized_file))
+    else:
+        stemmed_or_tokenized_dict = {}
+    original_file = "models/global-original_dict.json"
+    if os.path.isfile(original_file):
+        original_dict = json.load(file(original_file))
+    else:
+        original_dict = {}
+
+    for date in dates:
+        print "parsing month: " + date
+        for line in open("data/" + date + "-posts.tsv"):
+            [id, postDate, type, score, title, text, tags] = line.split('\t')
+            # stemmed_or_tokenized_line = utils.lemmatize(text.lower(), stopwords=stoplist)
+            stemmer = PorterStemmer()
+            stemmed_or_tokenized_line = [stemmer.stem(word) for word in word_tokenize(text.decode('utf-8'), language='english') if word not in stoplist]
+            stemmed_or_tokenized_dict[id] = stemmed_or_tokenized_line
+            original_dict[id] = text
+        dictionary = corpora.Dictionary(stemmed_or_tokenized_dict.values())
+        corpus = [dictionary.doc2bow(sentence) for sentence in stemmed_or_tokenized_dict.values()]
+        corpora.MmCorpus.serialize('models/' + date + '-tokenized.mm', corpus)  # store to disk, for later use
+
+    with open("models/global-tokenized_dict.json", 'w') as f:
+        f.write(json.dumps(stemmed_or_tokenized_dict))
+    with open("models/global-original_dict.json", 'w') as f:
+        f.write(json.dumps(original_dict))
+    dictionary = corpora.Dictionary(stemmed_or_tokenized_dict.values())
+    dictionary.compactify()
+    dictionary.save('models/global-dictionary.dict')
+
+
+# def fileBasedLemmatization(date, file):
+#     corpus = MyCorpus() # doesn't load the corpus into memory!
+#     corpora.MmCorpus.serialize('models/'+date+'-corpus.mm', corpus)
 
 
 if __name__ == '__main__':
