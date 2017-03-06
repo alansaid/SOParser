7ed8c068a993166f85db59892b690a2f3cc4401f
Alan Said <alan@alans.se>
2017-03-06 22:02:10 +0100

    updates

--- NormalizeText.py
+++ NormalizeText.py
@@ -1,100 +1,109 @@
+from __future__ import print_function
 from gensim import utils, corpora, models
 from gensim.parsing.preprocessing import STOPWORDS
-import logging, json, os
+import logging, json, os, re
 from pprint import pprint
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.ERROR)
 from nltk.stem.porter import PorterStemmer
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
+import numpy
 
-# import nltk
-
-
-# class MyCorpus(object):
-#     def __iter__(self):
-#         for line in open(file):
-#             dictionary = corpora.Dictionary.load('models/global-dictionary.dict')
-#             # stoplist = set(nltk.corpus.stopwords.words("english"))
-#             stoplist = STOPWORDS
-#             yield dictionary.doc2bow(utils.lemmatize(line.split('\t')[5].lower(), stopwords=stoplist))
-#
-
-# pick 5 users, track topics over
 
 def main():
-    dates = ['2013-01','2013-02']#, '2013-02']
+    dates = ['2013-01','2013-02','2013-03']#, '2013-02']
 
-    createGlobalDictionary()
-    memoryBasedTokenization(dates)
-    performTFIDF(dates)
-    performLDA(dates)
-
+    # filterUsers(dates)
+    # createGlobalDictionary(dates)
+    # memoryBasedTokenization(dates)
+    # performTFIDF(dates)
+    # performLDA(dates)
 
     # performHDP(date)
     # lookupHDPTopics(date, ids)
     lookupTopics(dates)
 
-    tid = 5
-
-    print "----\njan. topic id" + str(tid)
-    seeTopic('2013-01', tid)
-    tid = 16
-    print "----\njan. topic id" + str(tid)
-    seeTopic('2013-01', tid)
-    print "------------"
-    tid = 8
-    print "----\nfeb. topic id" + str(tid)
-    seeTopic('2013-02', tid)
-    tid = 11
-    print "----\nfeb. topic id" + str(tid)
-    seeTopic('2013-02', tid)
 
 
-    # print "January. topic id 16"
+def filterUsers(dates):
+    users = set()
+    for date in dates:
+        musers = set()
+        for line in open("data/" + str(date) + "-title-users.txt", "r"):
+            musers.add(line.strip("\n"))
+        if len(users) == 0:
+            users = musers
+        else:
+            users = set.intersection(users, musers)
+    ufile = open("data/title-users.txt", "w")
+    for user in users:
+        ufile.write(user + "\n")
+    ufile.close()
 
-    # seeTopic('2013-01', 16)
-
-
-def seeTopic(date, topicid):
-    lda = models.LdaModel.load("models/" + date + "-lda.model")
-    topic_terms = lda.get_topic_terms(topicid, topn=5)
-    dictionary = corpora.Dictionary.load("models/global-dictionary.dict")
-    print len(dictionary.keys())
-    for term in topic_terms:
-        print dictionary.get(term[0]) + "\t" + str(term[1])
 
 
 def lookupTopics(dates):
     tokenized_dict = json.load(file("models/global-tokenized_dict.json"))
     dictionary = corpora.Dictionary.load("models/global-dictionary.dict")
-
     users = set()
-    for user in open('2013-02-users.txt'):
-        users.add(user)
+    for user in open("data/title-users.txt"):
+        users.add(user.strip("\n"))
     document_users = {}
     document_scores = {}
     for date in dates:
-        documentfile = open("data/1k-" + str(date) + "-posts.tsv")
+        print(date)
+        usertopics = {}
+        userdoctopics = {}
+        usertopicscores = {}
+        documentfile = open("data/" + str(date) + "-titles-tags.tsv")
         topicfile = open(str(date) + "-topics.txt", 'a')
         lda = models.LdaModel.load("models/" + date + "-lda.model")
-        for doc in documentfile:
-            # line = rowID + '\t' + ownerUserID + '\t' + creationDate + '\t' + postTypeId  + '\t' + text + '\t' + score + '\n'
-            [docid, userid, creationdate, type, text, score] = doc.rstrip("\n").split("\t")
 
-            # if dates[0] in creationdate:
-            #     users.add(userid)
-            # elif dates[0] not in creationdate and userid not in users:
-            #     continue
+        for doc in documentfile:
+            [docid, userid, creationdate, score, title, tags] = doc.rstrip("\n").split("\t")
+
             if userid not in users:
                 continue
+
             document_users[docid] = userid
             document_scores[docid] = score
 
             sentence = tokenized_dict[docid]
             bow = dictionary.doc2bow(sentence)
-            topics = lda[bow]
-            topics_by_value = sorted(topics, key=lambda tup: tup[1], reverse=True)
-            topicfile.write(docid + "\t" + userid + "\t" + score + "\t" + json.dumps(topics_by_value) + "\n")
+            documenttopics = lda[bow]
+
+            # topics_by_value = sorted(documenttopics, key=lambda tup: tup[1], reverse=True)
+            # topicfile.write(docid + "\t" + userid + "\t" + score + "\t" + json.dumps(topics_by_value) + "\n")
+
+            for (topicid, topicvalue) in documenttopics:
+                if userid not in userdoctopics:
+                    userdoctopics[userid] = {}
+                    userdoctopics[userid][topicid] = []
+
+                    usertopicscores[userid] = {}
+                    usertopicscores[userid][topicid] = []
+
+                if topicid not in userdoctopics[userid]:
+                    userdoctopics[userid][topicid] = []
+                    usertopicscores[userid][topicid] = []
+
+                userdoctopics[userid][topicid].append(topicvalue)
+                usertopicscores[userid][topicid].append(int(score))
+        # topicfile.close()
+        for userid in userdoctopics.keys():
+            usertopics[userid] = {}
+            for topicid in userdoctopics[userid].keys():
+                meantopicvalue = numpy.mean(userdoctopics[userid][topicid])
+                meantopicscore = numpy.mean(usertopicscores[userid][topicid])
+                if meantopicvalue < 0.1:
+                    continue
+                usertopics[userid][topicid] = meantopicvalue
+                topicterms = lda.get_topic_terms(topicid, topn=5)
+                topicline = ""
+                for term in topicterms:
+                    topicline += dictionary.get(term[0]).ljust(15) + "\t"
+                resultline = str(userid)+"\t"+str(topicid)+"\t"+ str(meantopicvalue) + "\t" + topicline + "\t" + str(meantopicscore) + "\n"
+                topicfile.write(resultline)
         topicfile.close()
 
 
@@ -118,7 +127,7 @@ def lookupHDPTopics(date, docIDs):
 
         print(hdp[bow])
 
-    print hdp.show_topics(num_topics=-1, num_words=5, formatted=True)
+    print(hdp.show_topics(num_topics=-1, num_words=5, formatted=True))
 
 def lookupLDATopics(date, docIDs, numTopics):
     tokenized_dict = json.load(file("models/global-tokenized_dict.json"))
@@ -141,7 +150,7 @@ def performTFIDF(dates):
 
 def performLDA(dates):
     for date in dates:
-        print "performing lda"
+        print("performing lda on " + str(date))
         dictionary = corpora.Dictionary.load("models/global-dictionary.dict")
         corpus = corpora.MmCorpus("models/" + date + "-tfidf.mm")
         lda = models.LdaMulticore(corpus, id2word=dictionary, num_topics=20, workers=3)
@@ -151,7 +160,7 @@ def performLDA(dates):
         lda.save('models/' + date + '-lda.model')
 
 def performHDP(date):
-    print "performing lda"
+    print("performing hdp")
     dictionary = corpora.Dictionary.load("models/global-dictionary.dict")
     corpus = corpora.MmCorpus("models/" + date + "-tfidf.mm")
     hdp = models.HdpModel(corpus, id2word=dictionary)
@@ -159,34 +168,30 @@ def performHDP(date):
     corpora.MmCorpus.serialize('models/' + date + '-hdp.mm', hdp_corpus)
     hdp.save('models/' + date + '-hdp.model')
 
-def createGlobalDictionary():
-    years = [2013] #, 2014]
-    # stoplist = set(nltk.corpus.stopwords.words("english"))
+def createGlobalDictionary(dates):
     stoplist = STOPWORDS
     tokenized_dict = {}
     original_dict = {}
-    for year in years:
-        # months = [str(year) + "-" + str(item).zfill(2) for item in range(1, 13)]
-        months = ['2013-01','2013-02']
-        for month in months:
-            print "parsing month: " + month
-            linecounter = 0
-            for line in open("data/10k-" + month + "-posts.tsv"):
-                linecounter+=1
-                [id, userid, postDate, type, text, score] = line.split('\t')
-                stemmer = PorterStemmer()
-                tokenized_line = [stemmer.stem(word) for word in word_tokenize(text.decode('utf-8'), language='english') if word not in stoplist and len(word) > 3]
-
-                tokenized_dict[id] = tokenized_line
-                original_dict[id] = text
-                print "line: " + str(linecounter) + "\r",
+    for date in dates:
+        print("parsing month: " + date)
+        linecounter = 0
+        for line in open("data/" + date + "-titles-tags.tsv"):
+            linecounter+=1
+            [id, userid, postDate, score, text, tags] = line.split('\t')
+            text = text + " " + tags
+            stemmer = PorterStemmer()
+            tokenized_line = [stemmer.stem(word.lower()) for word in word_tokenize(text.decode('utf-8'), language='english') if word not in stoplist and len(word) > 3 and re.match('^[\w-]+$', word) is not None]
+            tokenized_dict[id] = tokenized_line
+            original_dict[id] = text
+            # print("line: " + str(linecounter) + "\r",)
 
     with open("models/global-tokenized_dict.json", 'w') as f: f.write(json.dumps(tokenized_dict))
     with open("models/global-original_dict.json", 'w') as f: f.write(json.dumps(original_dict))
     dictionary = corpora.Dictionary(tokenized_dict.values())
-    dictionary.filter_extremes(no_below=100, no_above=0.6, keep_n=500)
+    dictionary.filter_extremes(no_below=200, no_above=0.8, keep_n=1000)
     dictionary.compactify()
     dictionary.save('models/global-dictionary.dict')
+    print("Number of terms in dictionary: " + str(len(dictionary.keys())))
 
 def memoryBasedTokenization(dates):
     stoplist = set(stopwords.words("english"))
@@ -203,13 +208,14 @@ def memoryBasedTokenization(dates):
         original_dict = {}
 
     for date in dates:
-        print "parsing month: " + date
-        for line in open("data/10k-" + date + "-posts.tsv"):
+        print("parsing month: " + date)
+        for line in open("data/" + date + "-titles-tags.tsv"):
             # [id, postDate, type, score, title, text, tags] = line.split('\t')
-            [id, userid, postDate, type, text, score] = line.rstrip('\n').split('\t')
+            [id, userid, postDate, score, text, tags] = line.rstrip('\n').split('\t')
+            text = text + " " + tags
             # stemmed_or_tokenized_line = utils.lemmatize(text.lower(), stopwords=stoplist)
             stemmer = PorterStemmer()
-            stemmed_or_tokenized_line = [stemmer.stem(word) for word in word_tokenize(text.decode('utf-8'), language='english') if word not in stoplist and len(word) > 3]
+            stemmed_or_tokenized_line = [stemmer.stem(word.lower()) for word in word_tokenize(text.decode('utf-8'), language='english') if word not in stoplist and len(word) > 3]
             stemmed_or_tokenized_dict[id] = stemmed_or_tokenized_line
             original_dict[id] = text
         # dictionary = corpora.Dictionary(stemmed_or_tokenized_dict.values())
@@ -225,10 +231,6 @@ def memoryBasedTokenization(dates):
     # dictionary.compactify()
     # dictionary.save('models/global-dictionary.dict')
 
-
-# def fileBasedLemmatization(date, file):
-#     corpus = MyCorpus() # doesn't load the corpus into memory!
-#     corpora.MmCorpus.serialize('models/'+date+'-corpus.mm', corpus)
 
 
 if __name__ == '__main__':
--- UserPostCountExtractor.py
+++ UserPostCountExtractor.py
@@ -2,69 +2,68 @@
 # -*- coding: utf-8 -*-
 
 import xml.etree.ElementTree
+import json
 
 
 def main():
     minposts = 50
-    years = [2013, 2014]
+    years = [2013] #, 2014]
     # users = extractUsers(minposts, years)
     extractComments(years)
 
 def extractComments(years):
     users = set()
     usersFile = open('../userposts.txt')
+    questiontitles = {}
+    questiontags = {}
     for userline in usersFile:
         [user, number] = userline.strip().split('\t')
         users.add(user)
-
     for year in years:
         print "Parsing year: " + str(year)
-        months = [str(year) + "-" + str(item).zfill(2) for item in range(1,13)]
-
-        posts = open("../"+str(year)+"-Posts.xml", 'r')
-        postsFiles = {}
+        months = [str(year) + "-" + str(item).zfill(2) for item in range(1,4)]
         for month in months:
-            postsFiles[month] = open("data/"+ str(month) + "-posts.tsv","a")
-
-
-
-        for post in posts:
-            post = post.rstrip('\n')
-            if "row Id" not in post:
-                continue
-            doc = xml.etree.ElementTree.fromstring(post)
-            ownerUserID = doc.get('OwnerUserId')
-            if ownerUserID not in users:
-                continue
-            creationDate = doc.get('CreationDate')
-            rowID = doc.get('Id')
-            postTypeId = doc.get('PostTypeId')
-            # acceptedAnswerId = doc.get('AcceptedAnswerId')
-            score = doc.get('Score')
-            # viewCount = doc.get('ViewCount')
-            text = doc.get('Body').encode('utf8').replace('\r\n','').replace('\n','')
-            # lastEditorUserID = doc.get('LastEditorUserId')
-            # lastEditorDisplayName = doc.get('LastEditorDisplayName')
-            # lastEditDate = doc.get('LastEditDate')
-            # lastActivityDate = doc.get('LastActivityDate')
-            if 'Title' in doc:
-                title = doc.get('Title').encode('utf8')
-            else:
-                title = ''
-            if 'Tags' in doc:
-                tags = doc.get('Tags').encode('utf8')
-            else:
-                tags = ''
-            # answerCount = doc.get('AnswerCount')
-            # commentCount = doc.get('CommentCount')
-            # favoriteCount = doc.get('FavoriteCount')
-            # communityOwnedDate = doc.get('CommunityOwnedDate')
-            line = rowID + '\t' + creationDate + '\t' + postTypeId + '\t' + ownerUserID + '\t' + title + '\t' + text + '\t' + tags + '\n'
-
-            postsFiles[creationDate[0:7]].write(line)
-
-        for month in months:
-            postsFiles[month].close()
+            print month
+            postsFile = open("data/"+ str(month) + "-titles-tags.tsv","a")
+            posts = open("../" + str(month) + ".Posts.xml", 'r')
+            for post in posts:
+                post = post.rstrip('\n')
+                if "row Id" not in post:
+                    continue
+                doc = xml.etree.ElementTree.fromstring(post)
+                ownerUserID = doc.get('OwnerUserId')
+                if ownerUserID not in users:
+                    continue
+                creationDate = doc.get('CreationDate')
+                rowID = doc.get('Id')
+                postTypeId = doc.get('PostTypeId')
+                score = doc.get('Score')
+                text = doc.get('Body').encode('utf8').replace('\r\n','').replace('\n','')
+                parent = doc.get('ParentId')
+                if 'Title' in doc.keys():
+                    title = doc.get('Title').encode('utf8')
+                else:
+                    title = ''
+                if 'Tags' in doc.keys():
+                    tags = doc.get('Tags').encode('utf8').replace("><", ",").replace("<","").replace(">","")
+                else:
+                    tags = ''
+                if postTypeId == "1":
+                    questiontags[rowID] = tags
+                    questiontitles[rowID] = title
+                elif parent in questiontitles.keys() and parent in questiontags.keys() :
+                    tags = questiontags[parent]
+                    title = questiontitles[parent]
+                else:
+                    continue
+                # line = rowID + '\t' + ownerUserID + '\t' + creationDate + '\t' + title + '\t' + text + '\t' + tags + '\n'
+                line = rowID + '\t' + ownerUserID + '\t' + creationDate + '\t' + score + "\t" + title + '\t' + tags + '\n'
+                postsFile.write(line)
+            postsFile.close()
+    with open("questiontitles.json", 'w') as f:
+        f.write(json.dumps(questiontitles))
+    with open("questiontags.json", 'w') as f:
+        f.write(json.dumps(questiontags))
 
 
 def extractUsers(minPostCount, years):
